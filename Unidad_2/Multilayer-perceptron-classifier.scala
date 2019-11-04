276d04a548ca6777bf673058368770a8a306ca7d
rigoberto <rigoberto.hernandez17@tectijuana.edu.mx>
2019-11-03 21:00:45 -0800

    first commit from team five Multilayer perceptron classifier

---
+++ Unidad_2/Multilayer-perceptron-classifier.scala
@@ -0,0 +1,24 @@
+// $example on$
+//Loading required packages and APIs
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+// $example off$
+import org.apache.spark.sql.SparkSession
+
+/**
+ * An example for Multilayer Perceptron Classification.
+ */
+ ///Creating a Spark session
+ val spark = SparkSession.builder.appName("MultilayerPerceptronClassifierExample").getOrCreate()
+
+    // $example on$
+    // Load the data stored in LIBSVM format as a DataFrame.
+    //Load the input data in libsvm format.
+    val data = spark.read.format("libsvm").load("sample_multiclass_classification_data.txt")
+
+    // Split the data into train and test
+    //Preparing the training and testing set
+    //Prepare the train and test set: training => 60%, test => 40% and seed => 1234L
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+    val train = splits(0)
+    val test = splits(1)
