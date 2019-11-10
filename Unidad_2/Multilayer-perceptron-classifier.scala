14f18c9cc074b855227be892cc5766a919d4f480
Adrian Torres <tec.adriantf@gmail.com>
2019-11-09 19:05:09 -0800

    Update and rename Unidad_2/Multilayer-perceptron-classifier.scala to  Unidad_2/Multilayer-perceptron-classifier.scala
    Update, specify layers for the neural network

--- Unidad_2/Multilayer-perceptron-classifier.scala
+++ Unidad_2/Multilayer-perceptron-classifier.scala
@@ -22,3 +22,15 @@ import org.apache.spark.sql.SparkSession
     val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// specify layers for the neural network:
+    // input layer of size 4 (features), two intermediate of size 5 and 4
+    // and output of size 3 (classes)
+    val layers = Array[Int](4, 5, 4, 3)
+
+    // create the trainer and set its parameters
+    val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
+
+    // train the model
+    val model = trainer.fit(train)
